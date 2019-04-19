dfbafbc8e6d4288e8cb961c622b6422bd5e4882b
zengqiang <405039066@qq.com>
2019-04-19 18:31:30 +0800

    + new trunk
    + new trunk

---
+++ Assets/MeshEditor/ContorlPoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ContorlPoint : MonoBehaviour {
+
+    // Use this for initialization
+    public delegate void OnConrolPointMoveDelegate(string id, Vector3 pos);
+    public OnConrolPointMoveDelegate OnConrolPointMove = null;
+    Vector3 lastPosition;
+	void Start () {
+        lastPosition = transform.localPosition;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(lastPosition != transform.localPosition)
+        {
+            if(OnConrolPointMove != null)
+            {
+                OnConrolPointMove(name, transform.localPosition);
+            }
+            
+            lastPosition = transform.localPosition;
+        }
+	}
+}
